ec497bbce0d6481a9d598b750e442176499d7d54
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-15 20:26:09 +0530

    Gun Details Page

--- Guns/guns.js
+++ Guns/guns.js
@@ -13,26 +13,26 @@ document.addEventListener("DOMContentLoaded", () => {
       const gunDiv = document.createElement("div");
       gunDiv.classList.add("gun-item");
 
-      const gunDetailsDiv = document.createElement("div")
-      gunDetailsDiv.classList.add("gunDetailsDiv")
+      const gunDetailsDiv = document.createElement("div");
+      gunDetailsDiv.classList.add("gunDetailsDiv");
 
       const img = document.createElement("img");
       img.src = gun.displayIcon;
       img.alt = gun.displayName;
 
-      const heading = document.createElement('h1')
-      heading.classList.add("heading")
+      const heading = document.createElement("h1");
+      heading.classList.add("heading");
       heading.innerHTML = gun.displayName;
 
       const link = document.createElement("a");
       link.href = "#";
-      link.innerHTML = 'view';
+      link.innerHTML = "view";
       link.addEventListener("click", () => showGunDetail(gun.uuid));
 
       gunDiv.appendChild(img);
       gunDetailsDiv.appendChild(heading);
       gunDetailsDiv.appendChild(link);
-      gunDiv.appendChild(gunDetailsDiv)
+      gunDiv.appendChild(gunDetailsDiv);
       gunList.appendChild(gunDiv);
     });
   }
@@ -49,10 +49,18 @@ document.addEventListener("DOMContentLoaded", () => {
     gunList.classList.add("hidden");
     gunDetail.classList.remove("hidden");
     gunDetail.innerHTML = `
-        <h1>${gun.displayName}</h1>
+        <div class="gun-details-main">
+        <div class="gun-details-container">
+        <div class="gun-image" >
         <img src="${gun.displayIcon}" alt="${gun.displayName}" />
+        </div>
+        <div class="gun-details" >
+         <h1>${gun.displayName}</h1>
         <p>${gun.description}</p>
         <button id="back-button">Back</button>
+        </div>
+        </div>
+        </div>
       `;
 
     document.getElementById("back-button").addEventListener("click", () => {
